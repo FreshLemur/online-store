3e325e4de421273e444f7b97d00556e5c080ee95
FreshLemur <dimalimaredmi@gmail.com>
2024-08-14 15:44:56 +0300

    before add CartItem.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,34 +1,39 @@
 import { Link } from "react-router-dom";
 import styles from "./navBar.module.css";
 import cartIcon from "../assets/svg/cartIcon.svg";
+import { useSelector } from "react-redux";
+import { RootState } from "../store/store";
+
 export default function NavBar() {
+  const cartState = useSelector((state: RootState) => state.cartState.items);
+
   return (
     <header>
       <nav className={styles.navBar}>
         <ul className={styles.navList}>
           <li>
             <Link className={styles.navLink} to="/">
-              Home
+              Головна
             </Link>
           </li>
           <li>
             <Link className={styles.navLink} to="/products">
-              Products
+              Товари
             </Link>
           </li>
           <li>
             <Link className={styles.navLink} to="/cart">
-              Cart
+              Кошик
             </Link>
           </li>
           <li>
             <Link className={styles.navLink} to="/checkout">
-              Checkout
+              Оформлення замовлення
             </Link>
           </li>
           <section>
             <img src={cartIcon} alt="cartIcon" />
-            <span className={styles.cartQuantity}>0</span>
+            <span className={styles.cartQuantity}>{cartState.length}</span>
           </section>
         </ul>
       </nav>
--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -11,9 +11,6 @@ import { useDispatch, useSelector } from "react-redux";
 
 export default function Cart() {
   const cartState = useSelector((state: RootState) => state.cartState.items);
-  const cartStateQuantity = useSelector(
-    (state: RootState) => state.cartState.items
-  ).map((item) => item);
   const totalAmount = useSelector((state: RootState) => state.cartState.total);
   const dispatch = useDispatch();
 
@@ -21,9 +18,9 @@ export default function Cart() {
     e: ChangeEvent<HTMLInputElement>,
     id: number
   ) => {
-    console.log(cartState);
     const newValue = Number(e.target.value);
-    cartStateQuantity.forEach((item) => {
+
+    cartState.forEach((item) => {
       if (id === item.id) {
         if (newValue > item.quantity) {
           dispatch(
@@ -31,59 +28,72 @@ export default function Cart() {
               id,
             })
           );
-          console.log("More");
-        } else {
+        } else if (newValue < item.quantity) {
           dispatch(minusOneFromQuantity(id));
-          console.log("Less");
         }
       }
-      dispatch(totalPrice());
     });
+
+    dispatch(totalPrice());
   };
 
   const handleDelete = (id: number) => {
-    console.log(cartState);
     dispatch(removeItem(id));
     dispatch(totalPrice());
   };
 
   return (
     <div className={styles.cartWrapper}>
-      {cartState.map((cartState) => (
-        <div className={styles.cartProductWrapper} key={cartState.id}>
-          <img
-            className={styles.cartProductImage}
-            src={cartState.imageUrl}
-            alt={cartState.name}
-          />
-          <h2>{cartState.name}</h2>
+      {cartState.length > 0 ? (
+        cartState.map((item) => (
+          <>
+            <div className={styles.cartProductWrapper} key={item.id}>
+              <img
+                className={styles.cartProductImage}
+                src={item.imageUrl}
+                alt={item.name}
+              />
+              <h2>{item.name}</h2>
 
-          <section className={styles.cartQuantitySection}>
-            <label className={styles.cartQuantityLabel} htmlFor="cartQuantity">
-              Кількість:
-            </label>
-            <input
-              className={styles.cartQuantityInput}
-              type="number"
-              name="cartQuantity"
-              id="cartQuantity"
-              value={cartState.quantity}
-              onChange={(e) => quantityChangeHandler(e, cartState.id)}
-            />
-          </section>
+              <section className={styles.cartQuantitySection}>
+                <label
+                  className={styles.cartQuantityLabel}
+                  htmlFor="cartQuantity"
+                >
+                  Кількість:
+                </label>
+                <input
+                  className={styles.cartQuantityInput}
+                  type="number"
+                  name="cartQuantity"
+                  id="cartQuantity"
+                  value={item.quantity}
+                  onChange={(e) => quantityChangeHandler(e, item.id)}
+                />
+              </section>
 
-          <h2>Ціна: {cartState.price}</h2>
+              <h2>Ціна: {item.price}</h2>
 
-          <button
-            className={styles.cartDeleteButton}
-            onClick={() => handleDelete(cartState.id)}
-          >
-            Видалити
-          </button>
-          <div className={styles.totalQuantity}></div>
-        </div>
-      ))}
-      <h2>Загальна сума: {totalAmount}</h2>
+              <button
+                className={styles.cartDeleteButton}
+                onClick={() => handleDelete(item.id)}
+              >
+                Видалити
+              </button>
+              <div className={styles.totalQuantity}></div>
+            </div>
+          </>
+        ))
+      ) : (
+        <>
+          <h1 className={styles.emptyCartTitle}>
+            Ваш кошик порожній, додайте товари до кошику.
+          </h1>
+        </>
+      )}
+      {cartState.length > 0 && (
+        <h2 className={styles.totalAmount}>Загальна сума: {totalAmount}</h2>
+      )}
     </div>
   );
 }
